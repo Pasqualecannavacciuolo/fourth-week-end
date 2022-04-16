440b925c0342216c11d6456891cf7a1596aac30d
Pasquale <p.cannavacciuolo@icloud.com>
2022-04-16 18:22:55 +0200

    Added remaining files

---
+++ src/main/java/com/fourth/week/springfourthweek/SpringFourthWeekApplication.java
@@ -0,0 +1,13 @@
+package com.fourth.week.springfourthweek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFourthWeekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFourthWeekApplication.class, args);
+    }
+
+}
